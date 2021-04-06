9c1eac7db0bd841ee3ddfe47de05651e35dd8c8b
networkException <git@nwex.de>
2021-04-07 01:51:13 +0200

    Bundle: Disable dry run in sed command

--- bundle.sh
+++ bundle.sh
@@ -1,4 +1,4 @@
 tsc -p tsconfig.json
-sed '2d' dist/background.js
+sed -i '2d' dist/background.js # Remove Object.defineProperty(exports, "__esModule", { value: true });
 rm thundersort.zip
 zip thundersort.zip * 
