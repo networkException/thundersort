e9efbaff9fa1e32727180347cb105eecc73b1774
networkException <git@nwex.de>
2022-08-03 21:58:47 +0200

    Bundle: Limit the files and directories bundled

--- bundle.sh
+++ bundle.sh
@@ -4,6 +4,5 @@ tsc -p tsconfig.json
 sed -i '2d' dist/background.js # Remove Object.defineProperty(exports, "__esModule", { value: true });
 sed -i '2d' dist/options/index.js
 cp options/index.html dist/options/index.html
-cp LICENSE dist/LICENSE
 rm thundersort.zip
-zip -qqr thundersort.zip *
+zip -qqr thundersort.zip dist options LICENSE manifest.json README.md
